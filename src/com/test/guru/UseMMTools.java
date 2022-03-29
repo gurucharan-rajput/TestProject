9e17224158fc561a36edebcebcdfec69d28bbb0b
Gurucharan <grajput@erwin.com>
2022-03-29 17:37:59 +0530

    Updated Test Project

---
+++ src/com/test/guru/UseMMTools.java
@@ -0,0 +1,20 @@
+package com.test.guru;
+
+import org.apache.tools.ant.Project;
+
+import com.ads.api.util.MappingManagerUtil;
+
+public class UseMMTools {
+	
+	public static String test() {
+		// TODO Auto-generated method stub
+		
+		return "Test";
+	}
+	
+	public static String MMTest() {
+				
+		return "MMTest";
+	}
+
+}
